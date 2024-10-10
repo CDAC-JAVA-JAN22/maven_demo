9745cd126d7f81f701b620f74ad1750d293b6ada
Abhijeet Hemchandra Patil <97868310+AbhiP1894@users.noreply.github.com>
2024-10-10 15:51:36 +0530

    Update AppTest.java

--- src/test/java/com/example/app/AppTest.java
+++ src/test/java/com/example/app/AppTest.java
@@ -1,38 +1,38 @@
 package com.example.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    App calculator = new App();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testSubtract() {
+        assertEquals(2, calculator.subtract(5, 3));
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testMultiply() {
+        assertEquals(15, calculator.multiply(3, 5));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> calculator.divide(10, 0));
     }
 }
