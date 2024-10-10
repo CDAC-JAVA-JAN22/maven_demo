11c2274a7af8a2d43aa7237891c908d9ae2f36fe
Abhijeet Hemchandra Patil <97868310+AbhiP1894@users.noreply.github.com>
2024-10-10 15:50:54 +0530

    Update App.java

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -4,10 +4,36 @@ package com.example.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	    public int add(int a, int b) {
+	        return a + b;
+	    }
+
+	    public int subtract(int a, int b) {
+	        return a - b;
+	    }
+
+	    public int multiply(int a, int b) {
+	        return a * b;
+	    }
+
+	    public int divide(int a, int b) {
+	        if (b != 0) {
+	            return a / b;
+	        } else {
+	            throw new ArithmeticException("Cannot divide by zero");
+	        }
+	    }
+	    public boolean isPalindrome(String inputString) {
+	        if (inputString.length() == 0) {
+	            return true;
+	        } else {
+	            char firstChar = inputString.charAt(0);
+	            char lastChar = inputString.charAt(inputString.length() - 1);
+	            String mid = inputString.substring(1, inputString.length() - 1);
+	            return (firstChar == lastChar) && isPalindrome(mid);
+	        }
+	    }
+
 }
